afe52ef3db0c5e48cbaaa1bd6dbddece2a87599a
ykrods <890082+ykrods@users.noreply.github.com>
2025-09-05 21:55:52 +0900

    display warning when anchor positioning is not supported

--- src/pages/Dropdown2.svelte
+++ src/pages/Dropdown2.svelte
@@ -3,12 +3,19 @@
   import Dropdown from "$src/ui/dropdown/dropdown";
 
   let dropdown = $state()
+
+  const supported = CSS.supports('anchor-name: --x');
 </script>
 <svelte:head>
   <title>Dropdown2</title>
 </svelte:head>
 <main class="mt-12 mx-8">
   <h1 class="my-2 text-3xl font-bold">Dropdown2</h1>
+  {#if !supported}
+    <div role="alert" class="alert alert-error alert-soft my-3">
+      <span>It seems that your browser does not support anchor positioning. Display issues are likely to occur.</span>
+    </div>
+  {/if}
   <Dropdown.Root>
     <Dropdown.Button>dropdown</Dropdown.Button>
     <Dropdown.Menu bind:dropdown>
