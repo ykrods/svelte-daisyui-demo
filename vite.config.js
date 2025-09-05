2ae781e2e3b62c9d66eec1aec98411aa93707583
ykrods <890082+ykrods@users.noreply.github.com>
2025-09-05 20:29:37 +0900

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/svelte-daisyui-demo/",
   build: {
     minify: false,
     sourcemap: true,
